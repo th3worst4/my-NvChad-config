a3c15123d5d5860ba0136b6d1349a4da02fa8fc3
th3worst4 <caiocouto25@hotmail.com>
2025-02-16 22:37:25 -0300

    Added some keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("", "j", "gj")
+map("", "k", "gk")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -70,9 +70,10 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
+    build = "npm install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_browser = 'firefox'
     end,
     ft = { "markdown" },
   },
